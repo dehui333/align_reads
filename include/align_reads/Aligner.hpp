40ee66bb7d8b24c660991ae17fb00594bccf5da0
dehui333 <dehui333@gmail.com>
2022-06-17 13:50:56 +0800

    moved alignment_segment class to Aligner module

--- include/align_reads/Aligner.hpp
+++ include/align_reads/Aligner.hpp
@@ -29,6 +29,44 @@ namespace align_reads
             : query_start(q_start), target_start(t_start), query_len(q_len), target_len(t_len), mode(mode), task(task) {}
     };
 
+    /* Stores alignment info of a query w.r.t. to a target.
+     * Always exist w.r.t. to a target.
+     */
+    class alignment_segment
+    {
+    public:
+        // q_start/t_start are offsets from the start of query/target
+        // when aligning with edlib. Result should contain path.
+        // Frees(consumes) the edlib result.
+        alignment_segment(std::string &query, std::string &target,
+                          std::uint32_t q_start, std::uint32_t t_start, EdlibAlignResult &result);
+
+        // Get the characters (possibily '_' ) aligned to the target segment.
+        std::string &get_aligned_chars();
+
+        // Get the insertion segment at the specified index of the target.
+        // -1 for ins segment before 0.
+        std::string &get_ins_segment_at(int index);
+
+    private:
+        std::uint32_t start_on_target;     // The start index of the aligned segment on the target, 0-based
+        std::uint32_t align_len_on_target; // Align with how many from start (inclusive).
+        std::string aligned_chars;
+        std::vector<std::string> ins_segments;
+    };
+
+    // Stores info on how a set of sequences align to a single target.
+    class multi_alignment
+    {
+    public:
+        multi_alignment(std::string &target);
+        multi_alignment(std::string &&target);
+
+    private:
+        std::string target; // The target sequence
+        std::vector<alignment_segment> alignment_segments;
+    };
+
     // Process multiple edlib tasks, possibly in parallel
     std::vector<EdlibAlignResult> get_edlib_results(std::vector<edlib_task> &tasks, std::shared_ptr<thread_pool::ThreadPool> &pool);
 

6fd06a88197606d668e9518948d9485ecd79d462
dehui333 <dehui333@gmail.com>
2022-11-17 16:03:43 +0800

    fixed segfault - parallel creation of python objects

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -7,7 +7,7 @@
 #include "align_reads/Utilities.hpp"
 
 #include <iostream>
-
+#include <chrono>
 
 namespace align_reads
 {
@@ -34,6 +34,7 @@ namespace align_reads
     Generator::Generator(std::vector<std::string> &reads, std::shared_ptr<thread_pool::ThreadPool> pool, std::uint16_t window_length, bool debug_printing)
         : has_truth(false), pool(pool), inputs(1), overlapper(1, pool), window_length(window_length), debug_printing(debug_printing) 
     {
+        // 5808
         current_target = 0;
         inputs.append_to_group(READS_GROUP, reads, pool);
         inputs.index_group(READS_GROUP);
@@ -51,23 +52,52 @@ namespace align_reads
         return current_target < inputs.get_group(READS_GROUP).size();
     }
 
+    inline std::vector<PyObject*> get_target_info(std::string& target_name, std::uint32_t num_matrices) 
+    {
+        int int_num_matrices = static_cast<int>(num_matrices);
+        mtx.lock();
+        auto tuple = Py_BuildValue("si", target_name.c_str(), int_num_matrices);
+        mtx.unlock();
+        return {tuple};
+    }
+
     std::vector<std::vector<PyObject *>> Generator::produce_data()
     {
+        //auto p1 = std::chrono::steady_clock::now();
         std::uint32_t left_clip = 0;
         std::uint32_t right_clip = 0;
         std::vector<std::vector<PyObject *>> output;
-        //auto &target = inputs.get_id_in_group(READS_GROUP, current_target++);
-        auto &target = inputs.get_id_in_group(READS_GROUP, 28);
+        auto &target = inputs.get_id_in_group(READS_GROUP, current_target++);
+        //auto &target = inputs.get_id_in_group(READS_GROUP, 3319);
         std::string target_string = target->InflateData();
+        //auto p2 = std::chrono::steady_clock::now();
         AlignCounter align_counter = get_align_counter(target, target_string);
         std::uint32_t alignment_length = align_counter.alignment_length;
         std::uint32_t num_matrices = alignment_length % window_length == 0 ? alignment_length/window_length : alignment_length/window_length + 1;
+        //std::cout << 1 << std::endl;
+        //auto p3 = std::chrono::steady_clock::now();
+        /*
         if (has_truth)
         {
             output.push_back(get_ground_truth(target, target_string, align_counter, left_clip, right_clip, alignment_length, num_matrices));
-        }
+        }*/
+        //auto p4 = std::chrono::steady_clock::now();
+        //std::cout << 2 << std::endl;
         output.push_back(CountsConverter::get_counts_matrices(align_counter, window_length, left_clip, right_clip, num_matrices, alignment_length));
+        //auto p5 = std::chrono::steady_clock::now();
+        //std::cout << 3 << std::endl;
         output.push_back(CountsConverter::get_stats_matrices(align_counter, window_length, left_clip, right_clip, num_matrices, alignment_length));
+        //auto p6 = std::chrono::steady_clock::now();
+        //std::cout << 4 << std::endl;
+        output.push_back(get_target_info(target->name, num_matrices));
+        //std::cout << 5 << std::endl;
+        //auto p7 = std::chrono::steady_clock::now();
+        //std::cout << " 2- 1 " << std::chrono::duration_cast<std::chrono::microseconds>(p2 - p1).count() << std::endl;
+        //std::cout << " 3- 2 " << std::chrono::duration_cast<std::chrono::microseconds>(p3 - p2).count() << std::endl;
+        //std::cout << " 4- 3 " << std::chrono::duration_cast<std::chrono::microseconds>(p4 - p3).count() << std::endl;
+        //std::cout << " 5- 4 " << std::chrono::duration_cast<std::chrono::microseconds>(p5 - p4).count() << std::endl;
+        //std::cout << " 6- 5 " << std::chrono::duration_cast<std::chrono::microseconds>(p6 - p5).count() << std::endl;
+        //std::cout << " 7- 6 " << std::chrono::duration_cast<std::chrono::microseconds>(p7 - p6).count() << std::endl;
         return output;
     }
 
